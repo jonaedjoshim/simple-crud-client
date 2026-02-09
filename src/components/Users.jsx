4bdc507794a9bee9b6598b3f89f507af0d7e1586
mdjonaedjoshim <mdjonaedjoshim@gmail.com>
2026-02-09 23:19:38 +0600

    display user info on UI

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,9 +1,10 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 
 const Users = ({ userPromise }) => {
 
     const initialUsers = use(userPromise)
-    console.log(initialUsers)
+    const [users, setUsers] = useState(initialUsers)
+
     const handleAddUser = (e) => {
         e.preventDefault()
         const form = e.target
@@ -24,6 +25,9 @@ const Users = ({ userPromise }) => {
             .then(data => {
                 console.log('data after creating user in the db', data)
                 if (data.insertedId) {
+                    newUser._id = data.insertedId
+                    const newUsers = [...users, newUser]
+                    setUsers(newUsers)
                     alert('user added successfully')
                     form.reset()
                 }
@@ -35,16 +39,23 @@ const Users = ({ userPromise }) => {
     return (
         <div>
             {/* add user */}
-            <form onSubmit={handleAddUser} className="fieldset rounded-box w-lg space-y-2 p-4">
+            <form onSubmit={handleAddUser} className="fieldset border-2 border-gray-700 mb-6 rounded-box w-lg space-y-2 p-4">
 
-                <label className="label font-medium text-lg ">Name</label>
-                <input type="text" name='name' className="input w-full" placeholder="Name" />
+                <label className="label font-medium text-lg text-white">Name</label>
+                <input type="text" name='name' className="input w-full border-2 border-gray-700" placeholder="Name" />
 
-                <label className="label font-medium text-lg ">Email</label>
-                <input type="email" name='email' className="input w-full" placeholder="Email" />
+                <label className="label font-medium text-lg text-white">Email</label>
+                <input type="email" name='email' className="input w-full border-2 border-gray-700" placeholder="Email" />
 
                 <button className="btn btn-ghost border-gray-700 text-base p-5 border-2 mt-4 w-[50%]">Add User</button>
             </form>
+
+            {/* show users */}
+            <div className='border-2 border-gray-700 rounded flex flex-col gap-6 p-4'>
+                {
+                    users.map(user => <p className='border-2 border-gray-700 rounded p-4' key={user._id}>User Name : {user.name}<br />User Email : {user.email} <br/><button className='btn btn-ghost border-gray-700 text-base p-5 border-2 mt-4 w-[50%]'>Delete User</button></p>)
+                }
+            </div>
         </div>
     );
 };
