47146795a16dacca0d7b5ac2974d842c694f195b
mdjonaedjoshim <mdjonaedjoshim@gmail.com>
2026-02-09 21:08:29 +0600

    creating user

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Users from './components/Users'
 
 function App() {
 
   return (
-    <div>
-      <h2>Simple Crud Operation</h2>
+    <div className='w-11/12 mx-auto flex flex-col items-center mt-25 gap-12'>
+      <h2 className='font-bold text-3xl uppercase'>Simple Crud Operation</h2>
+      <Users />
     </div>
   )
 }
---
+++ src/components/Users.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Users = () => {
+
+    const handleAddUser = (e) => {
+        e.preventDefault()
+        const form = e.target
+        const name = form.name.value
+        const email = form.email.value
+        const user = { name, email }
+        console.log(user)
+
+        //create user in DB
+        fetch('http://localhost:3000/')
+        .then(res=>res.json)
+        .then(data=>{
+            consolele.log('data after creating user in the db', data)
+        })
+
+    }
+
+
+    return (
+        <div>
+            {/* add user */}
+            <form onSubmit={handleAddUser} className="fieldset rounded-box w-lg space-y-2 p-4">
+
+                <label className="label font-medium text-lg ">Name</label>
+                <input type="text" name='name' className="input w-full" placeholder="Name" />
+
+                <label className="label font-medium text-lg ">Email</label>
+                <input type="email" name='email' className="input w-full" placeholder="Email" />
+
+                <button className="btn btn-ghost border-gray-700 text-base p-5 border-2 mt-4 w-[50%]">Add User</button>
+            </form>
+        </div>
+    );
+};
+
+export default Users;
